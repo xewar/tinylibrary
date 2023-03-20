cf39c3f21a412ec5f73ccb754c04f58092c9f36c
xewar <92114989+xewar@users.noreply.github.com>
2023-03-20 15:21:33 -0400

    initial set up cont.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+/* GET users listing. */
+router.get("/cool", function (req, res, next) {
+  res.send(`practice new route`);
 });
 
 module.exports = router;
